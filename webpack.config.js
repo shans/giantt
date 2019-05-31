00a329639e19c48c8fe5b7d9a599f378945e850b
Cameron Silvestrini <csilvestrini@google.com>
2019-05-31 15:53:54 +1000

    Added HTML page, and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    library: 'giantt',
+    libraryTarget: 'window',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
